6f0de1052be7045f5ef44910620966248dc1bb84
Luka Jaković <ljakovi98@gmail.com>
2024-06-22 18:33:32 +0200

    Init project

---
+++ src/main/java/com/ljakovic/simple_products/SimpleProductsApplication.java
@@ -0,0 +1,13 @@
+package com.ljakovic.simple_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljakovic/simple_products/SimpleProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljakovic.simple_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
